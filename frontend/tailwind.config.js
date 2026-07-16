efb192ea35d811c9368f09ab65aae6d473a7a603
Suad Abrar <suadabrar44@gmail.com>
2026-07-16 22:57:00 +0300

    chore: add typography and icon dependencies

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,17 @@
+export default {
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: [
+          '"Noto Sans Ethiopic"',
+          "Nyala",
+          "Inter",
+          "ui-sans-serif",
+          "system-ui",
+          "sans-serif",
+        ],
+        en: ["Inter", "ui-sans-serif", "system-ui", "sans-serif"],
+      },
+    },
+  },
+};
